2fbd68fa1da6387826460968a68c69a09d746a6b
jswan <jswannieman@gmail.com>
2023-07-17 19:33:28 +1200

    Update App Javascript Component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  const [form, setState] = useState({
+    username: "",
+    password: "",
+  });
+
+  const printValues = (e) => {
+    e.preventDefault();
+    console.log(form.username, form.password);
+  };
+
+  const updateField = (e) => {
+    setState({
+      ...form,
+      [e.target.name]: e.target.value,
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form onSubmit={printValues}>
+      <label>
+        Username:
+        <input value={form.username} name="username" onChange={updateField} />
+      </label>
+      <br />
+      <label>
+        Password:
+        <input
+          value={form.password}
+          name="password"
+          type="password"
+          onChange={updateField}
+        />
+      </label>
+      <br />
+      <button>Submit</button>
+    </form>
   );
 }
 
